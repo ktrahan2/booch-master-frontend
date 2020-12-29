5529eb8b809d430a09e1b949064eb020f013b06c
Kyle Trahan <ktrahan2@gmail.com>
2020-12-29 13:12:00 -0700

    router set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route, Link, Switch } from "react-router-dom";
+import Home from "./components/Home/Home"
 import './App.css';
 
 function App() {
   return (
-    <div></div>
+    <main>
+      <Switch>
+        <Route path='/' component={Home} />
+      </Switch>
+    </main>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { rootReducer } from './redux/rootReducer';
-
+import { BrowserRouter } from 'react-router-dom';
 
 const store = createStore( rootReducer );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
